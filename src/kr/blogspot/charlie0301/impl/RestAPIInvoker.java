5e41adc7763f1206a0fa3f9f954859780a35b051
charles.kim <hallower@gmail.com>
2014-02-13 00:47:22 +0900

    - crash fixed when the network adpater is not available.

--- src/kr/blogspot/charlie0301/impl/RestAPIInvoker.java
+++ src/kr/blogspot/charlie0301/impl/RestAPIInvoker.java
@@ -24,7 +24,7 @@ public class RestAPIInvoker {
 	private final IWimpleImpl wimple;
 	public enum HTTP_METHOD { GET, POST, PUT, DELETE }
 
-	private static final boolean isNeedToPrintResult = true;
+	private static final boolean isNeedToPrintResult = false;
 	
 	public RestAPIInvoker(IWimpleImpl wimpleImpl){
 		this.wimple = wimpleImpl;
