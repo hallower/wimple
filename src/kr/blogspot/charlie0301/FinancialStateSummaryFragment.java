36c1d156c7d61f7e2523271668f8d9d6902e413e
charles.kim <hallower@gmail.com>
2014-02-27 00:19:36 +0900

    - add financial state fragments

---
+++ src/kr/blogspot/charlie0301/FinancialStateSummaryFragment.java
@@ -0,0 +1,84 @@
+package kr.blogspot.charlie0301;
+
+import kr.blogspot.charlie0301.WimpleActivity.CommandID;
+import kr.blogspot.charlie0301.impl.WimpleImpl;
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+public class FinancialStateSummaryFragment  extends Fragment implements IWimpleFragment{
+
+	private final static String LOG_TAG = "TransactionInsertFragment";
+
+	private final static WimpleImpl wimple = WimpleImpl.getInstance();
+	private WimpleActivity mainActivity = null;
+	private static View view = null;
+	private static Context context = null;
+
+
+
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		context = WimpleActivity.context;		
+
+		view = (FrameLayout)inflater.inflate(R.layout.fragment_finalcial_state_summary_tab, container, false);
+
+		// TODO Auto-generated method stub
+		return view;
+	}
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+	@Override
+	public void onDetach() {
+		// TODO Auto-generated method stub
+		super.onDetach();
+	}
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+	@Override
+	public void handleMessage(Message msg) {
+		int command = msg.what;
+		boolean booleanStatus = msg.arg1 == 1;
+		Object obj = msg.obj;
+
+		// if fragment is added or not to the activity
+		if(false == isAdded()){
+			return;
+		}
+
+		switch(command){
+
+		//case CommandID.WIMPLE_LOGGIN_SUCCESS :
+		case CommandID.GET_ALL_SECTION_RECEIVED :{
+			//initWimple();
+		}
+		break;
+		}
+	}
+	@Override
+	public void refreshView() {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void setActivityInstance(WimpleActivity instance) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
